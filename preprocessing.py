307c094daccb562147d90a7d168cdb6bd995ff2e
Rafael Dalzotto <rafaeldalzotto2016@gmail.com>
2020-08-05 21:22:33 -0300

    Dataset LISTO para el Clasificador con Keras

--- preprocessing.py
+++ preprocessing.py
@@ -6,17 +6,24 @@ noisy = pd.read_csv('NOISY_DATASET.CSV', sep=',', decimal='.') # index= [0,1,2]
 stable = pd.read_csv('STABLE_DATASET.CSV', sep=',', decimal='.') # index= [3,4,5]
 mov = pd.read_csv('MOV_DATASET.CSV', sep=',', decimal='.') # index= [6,7,8]
 
-isNoisy = [-1] * 250
-isStable = [0] * 250
-isMov = [1] * 250
+isTrue = [1] * 250
+isFalse = [0] * 250
 
-noisy['type'] = isNoisy
-stable['type'] = isStable
-mov['type'] = isMov
 
+noisy['noisy'] = isTrue
+noisy['stable'] = isFalse
+noisy['mov'] = isFalse
+
+stable['noisy'] = isFalse
+stable['stable'] = isTrue
+stable['mov'] = isFalse
+
+mov['noisy'] = isFalse
+mov['stable'] = isFalse
+mov['mov'] = isTrue
 
 
 result = pd.concat([noisy,stable,mov], ignore_index = True) #, keys=['x', 'y', 'z', 'type']
-result.to_csv('COMPLETE_DATASET.csv')
+# result.to_csv('COMPLETE_DATASET.csv') # ! Crear archivo [.CSV]
 
 print(result)
